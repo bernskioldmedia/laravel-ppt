f41c9a92f8beb7c82839f23d3976fd27f94063ce
Erik Bernskiold <erik@bernskioldmedia.com>
2024-02-16 12:37:30 +0100

    Update comment hint

--- src/Presentation/PresentationSlide.php
+++ src/Presentation/PresentationSlide.php
@@ -5,7 +5,7 @@ namespace BernskioldMedia\LaravelPpt\Presentation;
 use BernskioldMedia\LaravelPpt\Concerns\Makeable;
 
 /**
- * @method static BaseSlide make()
+ * @method static BaseSlide make(array $data)
  */
 abstract class PresentationSlide
 {
