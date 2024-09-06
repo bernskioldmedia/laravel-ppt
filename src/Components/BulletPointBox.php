3cdcd993d7ae45f8084a6f6bd3e380009f6bd234
Erik Bernskiold <erik@bernskioldmedia.com>
2024-09-06 11:44:06 +0200

    Add support for alignment and bullet color

--- src/Components/BulletPointBox.php
+++ src/Components/BulletPointBox.php
@@ -2,6 +2,7 @@
 
 namespace BernskioldMedia\LaravelPpt\Components;
 
+use BernskioldMedia\LaravelPpt\Concerns\Slides\WithAlignment;
 use BernskioldMedia\LaravelPpt\Presentation\BaseSlide;
 use PhpOffice\PhpPresentation\Style\Bullet;
 use PhpOffice\PhpPresentation\Style\Color;
@@ -11,9 +12,13 @@ use PhpOffice\PhpPresentation\Style\Color;
  */
 class BulletPointBox extends Component
 {
+    use WithAlignment;
+
     protected string $paragraphStyle = 'bulletPoint';
 
     protected string $bulletCharacter = '•';
+
+    protected ?string $bulletColor = null;
 
     protected int $spacingAfter = 20;
 
@@ -43,6 +48,13 @@ class BulletPointBox extends Component
         return $this;
     }
 
+    public function bulletColor(string $bulletColor): self
+    {
+        $this->bulletColor = $bulletColor;
+
+        return $this;
+    }
+
     public function spacingAfter(int $spacingAfter): self
     {
         $this->spacingAfter = $spacingAfter;
@@ -59,6 +71,8 @@ class BulletPointBox extends Component
                 $box = TextBox::make($this->slide, $bulletPoint)
                     ->paragraphStyle($this->paragraphStyle)
                     ->height($this->height)
+                    ->horizontalAlignment($this->horizontalAlignment)
+                    ->verticalAlignment($this->verticalAlignment)
                     ->width($this->width)
                     ->position($this->x, $this->y)
                     ->render()
@@ -77,7 +91,8 @@ class BulletPointBox extends Component
             $box->getActiveParagraph()
                 ->getBulletStyle()
                 ->setBulletType(Bullet::TYPE_BULLET)
-                ->setBulletChar($this->bulletCharacter);
+                ->setBulletChar($this->bulletCharacter)
+                ->setBulletColor(new Color($this->bulletColor ?? $this->slide->textColor));
 
             $box->getActiveParagraph()
                 ->setSpacingAfter($this->spacingAfter)
